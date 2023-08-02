c6f4a094d789a338c6d9952fb089bf556db1a7b3
Pushkar Deshmukh <pushkar2192@gmail.com>
2023-08-02 19:46:56 +0530

    Update base url

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,3 +1,4 @@
-const BASE_URL = "http://localhost:9080/api/v1";
+// const BASE_URL = "http://localhost:9080/api/v1";
+const BASE_URL = "https://blogify-api-v1-tgf4.onrender.com/api/v1";
 
 export default BASE_URL;
